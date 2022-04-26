0a8ac69faee1428ca325750b86005f7d921a8fae
EdouardNB <nade2903@usherbrooke.ca>
2022-04-26 17:31:56 -0400

    requete de reseau de collaboration

--- Requete SQL.R
+++ Requete SQL.R
@@ -1,9 +1,13 @@
 requete1<-function(){
-sql_requete<-"
-SELECT etudiant1 count(DISTINCT etudiant2) AS nb_collab
-FROM collaboration
-GROUP BY etudiant1;"
+con <- dbConnect(SQLite(), dbname="equipe.db")
+sql_requete <-"
+SELECT etudiant1,
+  count(DISTINCT etudiant2) AS nb_collab
+FROM collaborations
+GROUP BY etudiant1
+;"
 
-nb_collab<-dbGetQuery(,sql_requete)
+nb_collab<-dbGetQuery(con,sql_requete)
+write.csv(nb_collab, "donnees_BIO500/nbrecollaboration", row.names =  F)
 return(nb_collab)
 }

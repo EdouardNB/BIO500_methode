9a42895699534a067c5cca1e986adc8801ec8878
EdouardNB <nade2903@usherbrooke.ca>
2022-04-27 14:15:26 -0400

    function igraph

--- igraph.R
+++ igraph.R
@@ -1,4 +1,5 @@
 graphadj<-function(requetecollaboration){
-g <-  graph.adjacency(requetecollaboration)
+matrix<- table(requetecollaboration)
+g <-  graph.adjacency(matrix)
 plot(g, vertex.label = NA, edge.arrow.mode = 0, vertex.frame.color = NA, layout = layout_components(g))  
 }

d91dfe8d419f97bb8d5d5f5229600753da7f4d5f
NadiaTardy <n.tardy@hotmail.com>
2022-04-29 12:19:02 -0400

    ajouté le titre

--- diagrammesabande.R
+++ diagrammesabande.R
@@ -1,4 +1,5 @@
 bplot<-function(nbrecollaboration){
   pdf(file = "donnees_BIO500/diagram_collab.pdf")
   barplot(nbrecollaboration$nb_collab,xlab='Étudiants',ylab='Nombre de collaborations',ylim=c(0,35))
+  title(main='Nombre de collaborations des étudiants')
 }

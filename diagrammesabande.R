e85708af93465a14c0afceb445a950cae7e0b51f
Menuisix <menuisix@gmail.com>
2022-04-29 17:38:03 -0400

    rajout du noms des étudiants.

--- diagrammesabande.R
+++ diagrammesabande.R
@@ -4,8 +4,8 @@ bplot<-function(nbrecollaboration){
   title(main='Nombre de collaborations des etudiants')
 }
 
-bplot2<-function(nbrecollaboration){
+bplot2<-function(nbrecollaboration,list_BIO500){
   pdf(file = "donnees_BIO500/diagram_collab2.pdf")
-  barplot(nbrecollaboration[,2],xlab='Etudiants',ylab='Nombre de collaborations',ylim=c(0,35))
+  barplot(nbrecollaboration[,2],ylab='Nombre de collaborations',ylim=c(0,35), names.arg=list_BIO500, las=2, cex.names=0.4)
   title(main='Nombre de collaborations des etudiants')
 }

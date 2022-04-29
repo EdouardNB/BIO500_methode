95f01d519302ef372cf2ab0bc488c32d12a1a06c
NadiaTardy <n.tardy@hotmail.com>
2022-04-29 13:43:40 -0400

    modifié les titres et ajouté les etiquettes avec la frequ.

--- histogramme.R
+++ histogramme.R
@@ -1,3 +1,4 @@
 histo<-function(table_nbrecollab){
-  hist(table_nbrecollab$nb_collab)
+  h<-hist(table_nbrecollab$nb_collab,main='Fréquence du nombre de collaborations des étudiants',ylab = 'Fréquence',xlab='Nombre de collaborations',ylim=c(0,200))
+  text(h$mids,h$counts,labels=h$counts,pos=3,col=c('black',rep('black',3)))
 }

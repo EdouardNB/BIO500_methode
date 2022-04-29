a507ce0014746ed5d6a6f48252cd4a31d052a907
EdouardNB <nade2903@usherbrooke.ca>
2022-04-29 00:54:10 -0400

    if its broken fix it!

--- igraph.R
+++ igraph.R
@@ -1,5 +1,5 @@
-graphadj<-function(requetecollaboration){
-  pdf(file = "donnees_BIO500/igraphtry.pdf")
-g <-  graph.data.frame(requetecollaboration)
-plot(g)  
+graphadj<-function(table_collabo){
+g <-  graph.data.frame(table_collabo)
+png(file = "donnees_BIO500/igraph_etudiants.png")
+plot(g, vertex.label = NA, edge.arrow.mode = 0, vertex.frame.color = NA, layout = layout_components(g))  
 }

2e7f45c9791109702aab165be4d11bcd94146f2e
EdouardNB <nade2903@usherbrooke.ca>
2022-04-29 00:53:50 -0400

    if it not broken dont fix it

--- Requete SQL2.R
+++ Requete SQL2.R
@@ -6,5 +6,4 @@ SELECT etudiant1, etudiant2 FROM collaborations
   
 reseau_collab<-dbGetQuery(con,sql_requete2)
 write.csv(reseau_collab, "donnees_BIO500/requetecollaboration", row.names =  F)
-return(reseau_collab)
 }
